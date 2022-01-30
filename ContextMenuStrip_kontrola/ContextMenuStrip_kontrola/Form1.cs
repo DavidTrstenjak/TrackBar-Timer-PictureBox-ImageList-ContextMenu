de7f08c19074d52ce2fb19046732937f5cf8d938
DavidTrstenjak <90246263+DavidTrstenjak@users.noreply.github.com>
2022-01-30 14:03:51 +0100

    Add files via upload

---
+++ ContextMenuStrip_kontrola/ContextMenuStrip_kontrola/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContextMenuStrip_kontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cTRLDToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // klik na opciju LijevoDesno
+
+        private void lijevoDesnoToolStripMenuItem_Click(object sender, EventArgs e)
+
+        {
+
+            textBox1.Text = textBox2.Text;
+
+        }
+
+        // klik na opciju DesnoLijevo
+
+        private void desnoLijevoToolStripMenuItem_Click(object sender, EventArgs e)
+
+        {
+
+            textBox2.Text = textBox1.Text;
+
+        }
+    }
+}
